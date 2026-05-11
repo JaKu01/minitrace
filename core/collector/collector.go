24e9f21530ed9553e772beb1ae0e605d4e04e05e
JaKu01 <91499494+JaKu01@users.noreply.github.com>
2026-05-11 23:25:20 +0200

    Refactor api package.

--- core/collector/collector.go
+++ core/collector/collector.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"sync"
 
-	"github.com/JaKu01/minitrace/core/internal/api"
+	"github.com/JaKu01/minitrace/core/api"
 	"github.com/JaKu01/minitrace/core/trace"
 )
 
--- core/trace/handler.go
+++ core/trace/handler.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/JaKu01/minitrace/core/internal/api"
+	"github.com/JaKu01/minitrace/core/api"
 )
 
 type Handler struct{}
