5ccec814c36ec2e084a51330cc4159128b8800cf
JaKu01 <91499494+JaKu01@users.noreply.github.com>
2026-05-11 21:48:57 +0200

    Move go files to new repo structure.

---
+++ core/internal/trace/tracer.go
@@ -0,0 +1,46 @@
+package trace
+
+import (
+	"context"
+	"sync/atomic"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var Spans []*Span
+var serverStarted = atomic.Bool{}
+
+func EnsureServerStarted() {
+	if serverStarted.CompareAndSwap(false, true) {
+		go serveHttp()
+	}
+}
+
+func Start(ctx context.Context) (*Span, context.Context) {
+	EnsureServerStarted()
+
+	parentID := "root"
+
+	parent, ok := ctx.Value(activeSpanKey).(*Span)
+	if ok {
+		parentID = parent.ID
+	}
+
+	span := &Span{
+		ID:        uuid.NewString(),
+		ParentID:  parentID,
+		Name:      getCaller(),
+		StartTime: time.Now(),
+	}
+
+	Spans = append(Spans, span)
+
+	ctx = context.WithValue(ctx, activeSpanKey, span)
+	return span, ctx
+}
+
+func (s *Span) End() {
+	s.EndTime = time.Now()
+	s.Duration = s.EndTime.Sub(s.StartTime)
+}
